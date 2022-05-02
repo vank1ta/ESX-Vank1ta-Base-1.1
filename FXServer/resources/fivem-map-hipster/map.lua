98e5d77f6699b1424ab484b203ebd4bc6c2c8168
vank1ta <76558663+vank1ta@users.noreply.github.com>
2022-05-02 14:59:42 +0200

    Add files via upload

---
+++ FXServer/resources/fivem-map-hipster/map.lua
@@ -0,0 +1,71 @@
+vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 228.2736053466797 }
+
+
+
+
+
+spawnpoint 'a_m_y_hipster_01' { x = -267.84, y = -957.91, z = 31.22 }
+
+--[[spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }-9.96562,-1438.54,31.1015  -9.96562,-1438.54,31.1015
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_01' { x = -9.96562, y = -1438.54, z = 31.1015 }
+spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
+
+--]]
+--SERVER CREATED BY FIVEMBULGARIA.CF
