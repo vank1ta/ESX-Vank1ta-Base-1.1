b3d3602d38597eb9c1181ef5f64f6782e9c962ef
vank1ta <76558663+vank1ta@users.noreply.github.com>
2022-05-02 14:40:56 +0200

    Add files via upload

---
+++ FXServer/resources/[edit-scrips]/esx_inventoryhud/html/locales/en.js
@@ -0,0 +1,5 @@
+var invLocale = new Object();
+invLocale.dropItem = "Хвърли";
+invLocale.useItem = "Използвай";
+invLocale.giveItem = "Дай";
+invLocale.secondInventoryNotAvailable = "Втори инвентар не е наличен";
