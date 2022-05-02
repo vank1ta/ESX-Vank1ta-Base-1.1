6f3f2f9a7ff478ca94205f0be4bc4ace2b00d1f4
vank1ta <76558663+vank1ta@users.noreply.github.com>
2022-05-02 14:27:32 +0200

    Add files via upload

---
+++ FXServer/resources/[custom drehi]/roupas-invictus/__resource.lua
@@ -0,0 +1,8 @@
+files {
+  'mp_m_freemode_01_mp_m_rz_invictus.meta',
+  'mp_f_freemode_01_mp_f_rz_invictus.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_rz_invictus.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_rz_invictus.meta'
+
