11711e7268043d0c252e8e2bf913d36db6cf9caf
vank1ta <76558663+vank1ta@users.noreply.github.com>
2022-05-02 14:48:34 +0200

    Add files via upload

---
+++ FXServer/resources/[essentialmode]/essentialmode/client/main.lua
@@ -0,0 +1,67 @@
+--       Licensed under: AGPLv3        --
+--  GNU AFFERO GENERAL PUBLIC LICENSE  --
+--     Version 3, 19 November 2007     --
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		if NetworkIsSessionStarted() then
+			TriggerServerEvent('es:firstJoinProper')
+			TriggerEvent('es:allowedToSpawn')
+			return
+		end
+	end
+end)
+
+local oldPos
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1000)
+		local pos = GetEntityCoords(PlayerPedId())
+
+		if(oldPos ~= pos)then
+			TriggerServerEvent('es:updatePositions', pos.x, pos.y, pos.z)
+			oldPos = pos
+		end
+	end
+end)
+
+local myDecorators = {}
+
+RegisterNetEvent("es:setPlayerDecorator")
+AddEventHandler("es:setPlayerDecorator", function(key, value, doNow)
+	myDecorators[key] = value
+	DecorRegister(key, 3)
+
+	if(doNow)then
+		DecorSetInt(PlayerPedId(), key, value)
+	end
+end)
+
+AddEventHandler("playerSpawned", function()
+	for k,v in pairs(myDecorators)do
+		DecorSetInt(PlayerPedId(), k, v)
+	end
+
+	TriggerServerEvent('playerSpawn')
+end)
+
+playerMoving = false
+
+Citizen.CreateThread(function()
+	while true do Wait(0)
+		if not IsPedInAnyVehicle(PlayerPedId(), false) and GetEntitySpeed(PlayerPedId()) >= 0.5 and GetFollowPedCamViewMode() ~= 4 then
+			if playerMoving == false then
+				ShakeGameplayCam("ROAD_VIBRATION_SHAKE", 0.75)
+				playerMoving = true
+			end
+		else
+			if playerMoving == true then
+				StopGameplayCamShaking(false)
+				playerMoving = false
+			end
+		end
+	end
+end)
